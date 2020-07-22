4f3e7ca093279190b1d54526fb3766ec39269386
Pietro Sette <pietrosette@gmail.com>
2020-07-22 15:17:47 +0200

    jQuery done

---
+++ main.js
@@ -0,0 +1,11 @@
+//fa apparire la div hamburger-menu al click
+//sull'icona in alto a dx
+$('.fa-bars').click(function(){
+    $('.hamburger-menu').show();
+});
+
+//fa scomparire la div hamburger-menu al click 
+//sull'icona in alto a dx
+$('.fa-times').click(function(){
+    $('.hamburger-menu').hide();
+});
