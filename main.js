3c650af9c036090a03103d68e103650977aa5a62
Pietro Sette <pietrosette@gmail.com>
2020-07-22 17:42:07 +0200

    jQuery selectors revised 2.0

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 //fa apparire la div hamburger-menu al click
 //sull'icona in alto a dx
 $('a:last-of-type').click(function(){
-    $('.hamburger-menu').show();
+    $('.hamburger-menu').addClass('active');
 });
 
 //fa scomparire la div hamburger-menu al click
 //sull'icona in alto a dx
 $('.close').click(function(){
-    $('.hamburger-menu').hide();
+    $('.hamburger-menu').removeClass('active');
 });
